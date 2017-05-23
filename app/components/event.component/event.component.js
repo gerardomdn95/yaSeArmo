d37e6b750b93a861739aeee2dc33096b0d7fbc10
Gerardo Medina <gerardo.mdn95@gmail.com>
2017-05-22 21:52:13 -0500

    Hice mi componente event

---
+++ app/components/event.component/event.component.js
@@ -0,0 +1,16 @@
+(function(){
+    'use strict'
+
+var yaEvent = {
+    templateUrl: './app/components/event.component/event.component.html',
+    controller: eventCtrl
+};
+
+    angular
+        .module('yaApp')
+        .component('yaEvent', yaEvent);
+
+    function eventCtrl(){
+        var vm = this;
+    }
+})();
