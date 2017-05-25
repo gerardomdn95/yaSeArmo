f2c6fdef1749792a1890c898685bf0af10f35a47
moraelhumano <moraelhumano@gmail.com>
2017-05-25 01:19:16 -0500

    ya quedaron las rutas

--- app/components/friends.component/friends.component.js
+++ app/components/friends.component/friends.component.js
@@ -1,20 +1,30 @@
-(function(){
+(function () {
     'use strict'
 
-var yaFriends = {
-    templateUrl: './app/components/friends.component/friends.component.html',
-    controller: friendsCtrl
-};
-    angular
-        .module('yaApp')
-        .component('yaFriends', yaFriends);
 
-    friendsCtrl.$inject = ["usersJson"];
 
-    function friendsCtrl(usersJson){
+    var yaFriends = {
+        templateUrl:'app/components/friends.component/friends.component.html',
+        controller:friendsCtrl,
+    }
+
+
+    friendsCtrl.$inject = [ '$location', "usersJson"];
+
+    function  friendsCtrl($location,usersJson) {
+
         var users = this;
-        
+ 
         users.apiData = usersJson.query();
         console.log(users)
     }
+        
+    angular
+        .module('yaApp')
+        .component('yaFriends',yaFriends)
 })();
+
+
+
+
+
---
+++ app/routes.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict'
+
+    angular
+        .module('yaApp')
+        .config(config);
+
+    function config($routeProvider) {
+        $routeProvider
+            .when('/friends',{
+                template:'<ya-friends></ya-friends>'
+            })
+
+            .when('/friendsmo',{
+                template:'<h1>Hola Morros dodo<h1>'
+            })
+            
+            
+        
+            .otherwise({
+                redirectTo:'/friendsmo'
+            })
+    }
+})();
+
+
+ 
