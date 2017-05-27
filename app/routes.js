15979a11b69d616fbdc45bcc2dca8479e317ddb5
NellyDesu <nellydesu@gmail.com>
2017-05-26 20:35:19 -0500

    add ruta de evento

--- app/routes.js
+++ app/routes.js
@@ -13,18 +13,18 @@
                 template:'<ya-friends></ya-friends>'
             })
 
+            .when('/event',{
+                template:'<ya-event></ya-event>'
+            })
+
             .when('/friendsmo',{
                 template:'<h1>Hola Morros dodo<h1>'
             })
-            
-            
-        
+
+
+
             .otherwise({
                 redirectTo:'/friendsmo'
             })
     }
 })();
-
-
- 
-
