40419121a15f91864f50a57ba4f1608738ef335e
Gerardo Medina <gerardo.mdn95@gmail.com>
2017-05-27 15:19:28 -0500

    Corrección de jQuery

--- app/routes.js
+++ app/routes.js
@@ -6,42 +6,41 @@
         .module('yaApp')
         .config(config);
 
-    function config($routeProvider) {
+    function config($routeProvider, $locationProvider) {
         $routeProvider
 
-            .when('/home',{
-                template:'<ya-home></ya-home>'
+            .when('/home', {
+                template: '<ya-home></ya-home>'
             })
-            .when('/friends',{
-                template:'<ya-friends></ya-friends>'
-            })
-
-            .when('/yaevent',{
-                template:'<ya-event></ya-event>'
-            })
-            
-            .when('/yafriends',{
-                template:'<ya-friends></ya-friends>'
+            .when('/friends', {
+                template: '<ya-friends></ya-friends>'
             })
 
-            .when('/yaservices',{
-                template:'<ya-services></ya-services>'
-            })
-            .when('/yapacks',{
-                template:'<ya-packs></ya-packs>'
+            .when('/yaevent', {
+                template: '<ya-event></ya-event>'
             })
 
-            .when('/prueba',{
-                template:'<h1>Prosible vista</h1>'
+            .when('/yafriends', {
+                template: '<ya-friends></ya-friends>'
             })
-            .when('/yamap',{
-                template:'<ya-map></ya-map>'
+
+            .when('/yaservices', {
+                template: '<ya-services></ya-services>'
             })
-        
+            .when('/yapacks', {
+                template: '<ya-packs></ya-packs>'
+            })
+
+            .when('/prueba', {
+                template: '<h1>Prosible vista</h1>'
+            })
+            .when('/yamap', {
+                template: '<ya-map></ya-map>'
+            })
+
             .otherwise({
-                redirectTo:'/home'
-            })
+                redirectTo: '/home'
+            });
+            $locationProvider.html5Mode(true);
     }
-
-
 })();
