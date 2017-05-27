1f66c5619f49aab03e8cee00db3573980b4e9c9c
NellyDesu <nellydesu@gmail.com>
2017-05-27 11:20:01 -0500

    index

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,9 @@
             .when('/event',{
                 template:'<ya-event></ya-event>'
             })
+            .when('/friends',{
+                template:'<ya-friends></ya-friends>'
+            })
 
             .when('/friendsmo',{
                 template:'<h1>Hola Morros dodo<h1>'
