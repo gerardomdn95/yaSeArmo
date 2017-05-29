d7c275ae4746ef3fde71e616a994453e8a541ae6
moraelhumano <moraelhumano@gmail.com>
2017-05-28 23:03:22 -0500

    add drag and drop

--- app/components/friends.component/friends.component.js
+++ app/components/friends.component/friends.component.js
@@ -3,30 +3,31 @@
 
 
     var yaFriends = {
-        bindings: {
-            category: "="
-        },
-        templateUrl: './app/components/friends.component/friends.component.html',
-        controller: friendsCtrl
-    };
+        templateUrl:'app/components/friends.component/friends.component.html',
+        controller:friendsCtrl,
+    }
+
+
+    friendsCtrl.$inject = [ '$location', 'usersJson', '$scope'];
+
+    function  friendsCtrl($location,usersJson, $scope) {
+
+        var users = this;
+
+        users.apiData = usersJson.query();
+        console.log(users)
+
+        function activeController(){
+            $('#modal1').modal().modal('open');
+
+
+        }
+
+
+
+    }
 
     angular
         .module('yaApp')
-        .component('yaFriends', yaFriends);
-
-
-    friendsCtrl.$inject = [ '$location', "usersJson"];
-
-    function  friendsCtrl($location,usersJson) {
-
-        var users = this;
- 
-        users.apiData = usersJson.query();
-        console.log(users)
-    }
+        .component('yaFriends',yaFriends)
 })();
-
-
-
-
-
